178430ddfcbea5ad3ee12a0a1a1d5bf9f33ac64a
jkrusinski <jkruzr@gmail.com>
2017-02-27 17:26:11 -0600

    All tests passing

--- app/models/user.js
+++ app/models/user.js
@@ -5,6 +5,21 @@ var Promise = require('bluebird');
 
 
 var User = db.Model.extend({
+  tableName: 'users',
+  hasTimestamps: true,
+
+  initialize: function() {
+
+    this.on('creating', function(model, attrs, options) {
+      var hash = bcrypt.hashSync(model.get('password'));
+      model.set('password', hash);
+    });
+
+
+
+
+  }
+  
 });
 
 module.exports = User;
--- shortly.js
+++ shortly.js
@@ -10,38 +10,55 @@ var User = require('./app/models/user');
 var Links = require('./app/collections/links');
 var Link = require('./app/models/link');
 var Click = require('./app/models/click');
+var bcrypt = require('bcrypt-nodejs');
+var session = require('cookie-session');
+
 
 var app = express();
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(partials());
+//Session verification modules
+app.set('trust proxy', 1);
+app.use(session({
+  name: 'session',
+  keys: ['mysecret']
+}));
+
 // Parse JSON (uniform resource locators)
 app.use(bodyParser.json());
 // Parse forms (signup/login)
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
+var isLoggedIn = function(req, res, next) {
+  if (req.session.loggedIn) {
+    next();
+  } else {
+    res.redirect('/login');
+  }
+};
 
-app.get('/', 
-function(req, res) {
+app.get('/', isLoggedIn, function(req, res) {
   res.render('index');
 });
 
-app.get('/create', 
-function(req, res) {
+app.get('/example', function(req, res) {
+  res.send('<h1>This is some html</h1><a href="google.com">link</a>');
+});
+
+app.get('/create', isLoggedIn, function(req, res) {
   res.render('index');
 });
 
-app.get('/links', 
-function(req, res) {
+app.get('/links', isLoggedIn, function(req, res) {
   Links.reset().fetch().then(function(links) {
     res.status(200).send(links.models);
   });
 });
 
-app.post('/links', 
-function(req, res) {
+app.post('/links', isLoggedIn, function(req, res) {
   var uri = req.body.url;
 
   if (!util.isValidUrl(uri)) {
@@ -76,7 +93,72 @@ function(req, res) {
 // Write your authentication routes here
 /************************************************************/
 
+app.post('/login', function (req, res) {
 
+  var loginInfo = req.body;
+
+  new User({username: loginInfo.username}).fetch().then(function (user) {
+    if (user) {
+      bcrypt.compare(loginInfo.password, user.attributes.password, function(err, matched) {
+        if (err) {
+          throw err;
+        }
+
+        //set session to logged in
+        if (matched) {
+          req.session.loggedIn = true;
+          req.session.username = loginInfo.username;
+          res.redirect('/');
+        } else {
+          res.redirect('/login');
+        }
+      });
+    } else {
+      res.redirect('/login');
+    }
+  });
+
+
+});
+
+app.get('/login', function (req, res) {
+
+  res.render('login');
+
+});
+
+app.post('/signup', function (req, res) {
+
+  console.log('Body: ', req.body);
+
+  new User({username: req.body.username}).fetch().then(function (user) {
+    if (user) {
+      res.status(409).send('User Already Exists');
+    } else {
+
+      Users.create({
+        username: req.body.username,
+        password: req.body.password
+      })
+      .then(function(newUser) {
+        req.session.loggedIn = true;
+        req.session.username = req.body.username;
+        res.redirect('/');       
+      });
+    }
+  });
+});
+
+app.get('/signup', function(req, res) {
+  res.render('signup');
+});
+
+app.get('/logout', function(req, res) {
+
+  req.session.loggedIn = false;
+  delete req.session.username;
+  res.redirect('/login');
+});
 
 /************************************************************/
 // Handle the wildcard route last - if all other routes fail
